8323ef5443246885577a4878cd6574eb5cd9e6dc
Annie <annienero93@gmail.com>
2018-08-16 19:25:27 -0400

    create rating model

---
+++ src/main/java/com/example/cs4550finalprojectjava/models/Rating.java
@@ -0,0 +1,57 @@
+package com.example.cs4550finalprojectjava.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+enum RatingType {
+    PRODUCTION
+}
+@Entity
+public class Rating {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private RatingType ratingType;
+    private int ratingValue;
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name="review_id")
+    private Review review;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RatingType getRatingType() {
+        return ratingType;
+    }
+
+    public void setRatingType(RatingType ratingType) {
+        this.ratingType = ratingType;
+    }
+
+    public int getRatingValue() {
+        return ratingValue;
+    }
+
+    public void setRatingValue(int ratingValue) {
+        this.ratingValue = ratingValue;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+
+    public void updateRating(Rating rating) {
+        this.ratingValue = rating.ratingValue;
+    }
+}
+
