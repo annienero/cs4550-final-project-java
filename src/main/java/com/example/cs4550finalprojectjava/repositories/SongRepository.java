90e06a9500ca94bc00e7209d9a988d1b45d59499
Annie <annienero93@gmail.com>
2018-08-16 19:26:23 -0400

    create song repository

---
+++ src/main/java/com/example/cs4550finalprojectjava/repositories/SongRepository.java
@@ -0,0 +1,16 @@
+package com.example.cs4550finalprojectjava.repositories;
+
+import com.example.cs4550finalprojectjava.models.Song;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SongRepository extends CrudRepository<Song, Integer> {
+    @Query(value = "SELECT * FROM Song WHERE title LIKE %:keyword%", nativeQuery = true)
+    List<Song> findAllSongsWithKeyword(@Param("keyword") String keyword);
+
+    @Query("SELECT song FROM Song song WHERE song.songId=:songId")
+    Song findSongBySongId(String songId);
+}
