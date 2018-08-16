4af5d38735bd039d458adc87f966d42e8aecabb0
Annie <annienero93@gmail.com>
2018-08-16 18:33:16 -0400

    create user model

---
+++ src/main/java/com/example/cs4550finalprojectjava/models/User.java
@@ -0,0 +1,139 @@
+package com.example.cs4550finalprojectjava.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+enum Role {
+    REVIEWER,
+    ARTIST,
+    ADMIN
+}
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    @OneToMany(mappedBy="user")
+    private List<Review> reviews;
+    @OneToMany()
+    @JoinColumn(name="artist_id", referencedColumnName="id")
+    private List<Song> uploads;
+    @OneToMany()
+    @JoinColumn(name="commenter_id", referencedColumnName="id")
+    private List<Comment> comments;
+    @ManyToMany()
+    @JoinColumn(name="followed_id", referencedColumnName="id")
+    private List<User> followers;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Song> getUploads() {
+        return uploads;
+    }
+
+    public void setUploads(List<Song> uploads) {
+        this.uploads = uploads;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<User> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(List<User> followers) {
+        this.followers = followers;
+    }
+
+    public void updateUser(User user) {
+        if (user.username != null) {
+            this.username = user.username;
+        }
+        if (user.password != null) {
+            this.password = user.password;
+        }
+        if (user.firstName != null) {
+            this.firstName = user.firstName;
+        }
+        if (user.lastName != null) {
+            this.lastName = user.lastName;
+        }
+        if (user.email != null) {
+            this.email = user.email;
+        }
+    }
+}
