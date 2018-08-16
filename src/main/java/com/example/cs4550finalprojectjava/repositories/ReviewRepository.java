9311b9bb32b64f26d0ad061a10f831d45d7a31d9
Annie <annienero93@gmail.com>
2018-08-16 19:26:15 -0400

    create review repository

---
+++ src/main/java/com/example/cs4550finalprojectjava/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.example.cs4550finalprojectjava.repositories;
+
+import com.example.cs4550finalprojectjava.models.Review;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
+
+}
+
