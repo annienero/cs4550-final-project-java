5d6114f759620a6233e130e7a0c8d2511af5b20b
Annie <annienero93@gmail.com>
2018-08-16 19:26:05 -0400

    create rating repository

---
+++ src/main/java/com/example/cs4550finalprojectjava/repositories/RatingRepository.java
@@ -0,0 +1,8 @@
+package com.example.cs4550finalprojectjava.repositories;
+
+import com.example.cs4550finalprojectjava.models.Rating;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RatingRepository extends CrudRepository<Rating, Integer> {
+
+}
