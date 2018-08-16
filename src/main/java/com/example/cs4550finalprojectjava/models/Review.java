bb800e82164eced8fdd148f797f94a6478aee2cd
Annie <annienero93@gmail.com>
2018-08-16 19:25:41 -0400

    create review model

---
+++ src/main/java/com/example/cs4550finalprojectjava/models/Review.java
@@ -0,0 +1,98 @@
+package com.example.cs4550finalprojectjava.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date reviewTime;
+    private String reviewText;
+    @OneToMany()
+    @JoinColumn(name="review_id", referencedColumnName="id")
+    private List<Rating> ratings;
+    @OneToMany()
+    @JoinColumn(name="review_id", referencedColumnName="id")
+    private List<Comment> comments;
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name="song_id")
+    private Song song;
+
+    @PrePersist
+    protected void onCreate() {
+        reviewTime = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getReviewTime() {
+        return reviewTime;
+    }
+
+    public void setReviewTime(Date reviewTime) {
+        this.reviewTime = reviewTime;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public void updateReview(Review review) {
+        if (review.reviewText != null) {
+            this.reviewText = review.reviewText;
+        }
+    }
+}
