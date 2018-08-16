8bc68c93c7fe451efa0ce65048c628b2fb81c35a
Annie <annienero93@gmail.com>
2018-08-16 19:25:33 -0400

    create comment model

---
+++ src/main/java/com/example/cs4550finalprojectjava/models/Comment.java
@@ -0,0 +1,68 @@
+package com.example.cs4550finalprojectjava.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date commentTime;
+    private String commentText;
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+    @ManyToOne
+    @JsonIgnore
+    private Review review;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCommentTime() {
+        return commentTime;
+    }
+
+    public void setCommentTime(Date commentTime) {
+        this.commentTime = commentTime;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void updateComment(Comment comment) {
+        if (comment.commentText != null) {
+            this.commentText = comment.commentText;
+        }
+    }
+}
